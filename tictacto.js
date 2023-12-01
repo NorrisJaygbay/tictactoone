e3fe9c39378a64b7077ac071d51d16b3de512ecc
NorrisJaygbay <58750209+NorrisJaygbay@users.noreply.github.com>
2023-12-01 23:19:09 +0000

    Update tictacto.js

--- tictacto.js
+++ tictacto.js
@@ -57,6 +57,7 @@ function cheakWin(){
 function cheakDraw(){
     if(!isGameOver){
         let isDraw = true;
+        main_grid.classList.add('main-gridone');
         boxes.forEach(e =>{
             if(e.innerHTML ==="") isDraw = false;
             e.style.removeProperty("background-color");
